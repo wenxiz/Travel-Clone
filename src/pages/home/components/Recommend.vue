dc11891003807512a445ee22ed1790ba96b9a2be
zhouwenxi <zhouwenxi@ZhouwenxideMacBook-Pro.local>
2021-05-24 18:00:33 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li
+              class="item border-bottom"
+              v-for="item of recommendList"
+              :key="item.id"
+            >
+                <img class="item-img" :src="item.imgUrl" />
+                <div class="item-info">
+                    <p class="item-title">故宫</p>
+                    <p class="item-desc"> 扫ID叫嚣 is 京东埃及哦</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+        title: '故宫',
+        desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    background: #eee
+    line-height: .8rem
+    text-indent: .2rem
+  .item
+    display: flex
+    height: 1.9rem
+    .item-img
+      display: block
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .1rem
+        border-radius: .06rem
+</style>

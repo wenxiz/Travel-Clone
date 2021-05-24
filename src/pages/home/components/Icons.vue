07b6e445460039262dc76d93c86b9f51e7cc57d2
zhouwenxi <zhouwenxi@ZhouwenxideMacBook-Pro.local>
2021-05-24 17:29:19 +0800

    icons added

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="icons">
+      <swiper>
+        <swiper-slide
+          v-for="(page, index) of pages"
+          :key="index"
+        >
+          <div class="icon"
+            v-for="(item, index) of page"
+            :key="index"
+          >
+            <div class="icon-img">
+              <img :src="item.imgUrl" class="icon-img-content">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-wrapper
+    height: 0
+    padding-bottom: 50%
+  .icons
+    margin-top: .1rem
+    .icon
+      float: left
+      overflow: hidden
+      height: 0
+      width: 25%
+      padding-bottom: 25%
+      position: relative
+      .icon-img
+        position: absolute
+        top: 0
+        right: 0
+        left: 0
+        bottom: .44rem
+        padding: .1rem
+        box-sizing: border-box
+        .icon-img-content
+          display: block
+          height: 100%
+          margin: 0 auto
+      .icon-desc
+        position: absolute
+        right: 0
+        left: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $textDarkColor
+        ellipsis()
+</style>
